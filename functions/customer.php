9b5b8e9b0639d180414efaf916ceaad815be68de
Brin Brody <825orion@gmail.com>
2020-05-26 13:46:46 -0400

    Build some basic customer function

---
+++ functions/customer.php
@@ -0,0 +1,22 @@
+<?php
+require_once "../include/db.php";
+
+//Return 0 when customer is in queue, and 1 when customer is in store, -1 if customer not found.
+function findCustomer($id){
+    $customer = $db->query("SELECT `location` FROM `customers` WHERE `id`=$id")->fetch_object();
+    return ($customer==null ? -1 : $customer->location);
+}
+
+//Either move the user to their next position, or create them if they do not exist
+//Use case for known missing users: moveAlong(-1,someNameHere)
+function moveAlong($id, $name=""){
+    $customer = $db->query("SELECT `location` FROM `customers` WHERE `id`=$id")->fetch_object();
+    //Customer doesn't exist? No problem; we'll make one and stick them on the queue.
+    if($customer == null){
+        $db->query("INSERT INTO `customers` (`name`,`location`) VALUES ($name,0)");
+    }
+    //If the user already exists, we can just increase their location by 1
+    $db->query("UPDATE `customers` SET `location`=".($customer->location)+1);
+}
+
+?>
