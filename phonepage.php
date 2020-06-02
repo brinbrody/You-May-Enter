0a166cc900042f43e5e1765700dc2c6a69eb27d3
lamuelsee <50895332+lamuelsee@users.noreply.github.com>
2020-06-02 23:50:28 +0900

    uh just uploading what i got so far i spent a few hours on it lol its literally nothing so far

---
+++ phonepage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" class="background">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>You May Enter</title>
+
+  <link rel="stylesheet" type="text/css" href="css/phone-page.css">
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
