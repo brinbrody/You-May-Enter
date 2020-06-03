60fefb171fc1b48c6dbf1da29d9e71ace117c480
lamuelsee <50895332+lamuelsee@users.noreply.github.com>
2020-06-03 19:51:09 +0900

    lmao save me

--- index.php
+++ index.php
@@ -14,11 +14,19 @@
 <body>
   <div class="mainContainer">
     <div>
-      <h1 class="mainContainerTitle">You May Enter</h1> 
+      <h1 class="mainContainerTitle">You May Enter</h1>
       <div class="mainContainerText">
-        <div>help brin we should add some text here lol get me into college idiot xd hehe xd im gonna just keep typing so that i could fill this div up haha please help me im going insane wqeuwaijg weeewooooo weeeeeewooooooo</div>
-        <br>
-        <button type="button" class="continueButton">Continue</button>
+        <form action="/action_page.php">
+          <br><br>
+          <img src="icons/person-circle.svg" class="sizeIcon">
+          <input type="text" id="username" name="username" class="inputs" placeholder="Username"><br>
+          <img src="icons/lock.svg" class="sizeIcon">
+          <input type="password" id="password" name="pwd" class="inputs" placeholder="Password"><br><br>
+          <div class="centerText">
+            <input type="submit" value="Log In" class="signInButton">
+            <input type="submit" value="Register" class="registerButton">
+          </div>
+        </form>
       </div>
     </div>
   </div>
