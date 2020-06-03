4360ad501673f781b3b7579291c793af99343793
lamuelsee <50895332+lamuelsee@users.noreply.github.com>
2020-06-03 23:53:13 +0900

    pepega

--- Phone_webpages/waiting_page.php
+++ Phone_webpages/waiting_page.php
@@ -14,10 +14,10 @@
 <body>
   <div class="mainContainer">
     <div class="mainContainerText">
-      <div class="thanksParagraph">Hello, __________. Once it is your turn, please show this QR code at the door.</div>
+      <div class="thanksParagraph">Hello __________. Once it is your turn, please show this QR code at the door.</div>
       <img src="../img/1200px-QR_code_for_mobile_English_Wikipedia.svg.webp" class="qrCode">
-      <div class="progressBar"></div>
-      <br><br><div class="positionInLine">Position in Line: __</div>
+      <!-- <div class="progressBar"></div> -->
+      <br><div class="positionInLine">Position in Line: __</div><br> <!-- if implementing progress bar, take out the br at end of the div and place before the div -->
       <button type="button" class="getOffTheLine">Get off the Line</button>
     </div>
   </div>
