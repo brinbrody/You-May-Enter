05427b073259b64734018128d2f028cd2e079116
lamuelsee <50895332+lamuelsee@users.noreply.github.com>
2020-06-03 20:53:06 +0900

    phone landing page is done

--- phonepage.php
+++ phonepage.php
@@ -4,18 +4,29 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
 
   <title>You May Enter</title>
 
   <link rel="stylesheet" type="text/css" href="css/phone-page.css">
+  <link href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <body>
-
-
-
+  <div class="mainContainer">
+    <div>
+      <div class="mainContainerText">
+        <h1 class="mainContainerTitle">You May Enter</h1>
+        <form action="/action_page.php">
+          <br>
+          <img src="icons/person-circle.svg" class="sizeIcon">
+          <input type="text" id="username" name="username" class="inputs" placeholder="Your Name"><br>
+          <div class="centerText">
+            <input type="submit" value="Get in Line" class="submitButton">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </body>
 
 </html>
