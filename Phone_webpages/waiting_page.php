bcddaac27955b9c850e1e21d1f838c5e452e4104
Brin Brody <825orion@gmail.com>
2020-06-08 13:46:47 -0400

    Update refresh time

--- Phone_webpages/waiting_page.php
+++ Phone_webpages/waiting_page.php
@@ -18,7 +18,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="refresh" content="30">
+  <meta http-equiv="refresh" content="5">
   <title>You May Enter</title>
 
   <link rel="stylesheet" type="text/css" href="../css/waiting_page.css">
